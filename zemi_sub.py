c16a2d132b6a2f6a7785e67e67f2438471a9abbb
shota69123 <34471163+shota69123@users.noreply.github.com>
2017-12-13 16:31:29 +0900

    Update zemi_sub.py

--- zemi_sub.py
+++ zemi_sub.py
@@ -6,17 +6,16 @@ Created on Wed Nov 29 16:04:55 2017
 """
 
 import rospy
-import math
+import tf
 from sensor_msgs.msg import imu
+
+def quaternion_to_eurler(quaternion):
+    e = tf.transformations.euler_from_quaternion((quaternion.x, quaternion.y, quaternion.z, quaternion.w))
+    return Vector3(x=e[0], y=e[1], z=e[2])
+
 def callback(data):
-    #rospy.loginfo("{0:f}, {1:f}, {2:f}",format(data.liner_acceleration))
-    x = data.liner_acceleration[0]
-    y = data.liner_acceleration[1]
-    z = data.liner_acceleration[2]
-    theta_x = math.atan(x*math.pi/math.sqrt(y**2+z**2)*180)
-    theta_y = math.atan(x*math.pi/math.sqrt(z**2+x**2)*180)
-    theta_z = math.atan(x*math.pi/math.sqrt(x**2+y**2)*180)
-    rospy.loginfo(theta_x, theta_y, theta_z)
+    Vector3 = quaternion_to_eurler(data.orientation)
+    rospy.loginfo(x, y, z)
 
 def listener():
     rospy.init_node('listener', anoymous=True)
